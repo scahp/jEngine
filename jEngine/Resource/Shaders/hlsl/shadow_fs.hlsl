9aa47039d985c3a4291dac572cd937ccd64595b9
scahp <scahp@naver.com>
2022-08-03 17:38:46 +0900

    remove validation error

--- jEngine/Resource/Shaders/hlsl/shadow_fs.hlsl
+++ jEngine/Resource/Shaders/hlsl/shadow_fs.hlsl
@@ -3,7 +3,7 @@ struct VSOutput
     float4 Pos : SV_POSITION;
 };
 
-float4 main(VSOutput input) : SV_TARGET
+void main(VSOutput input) : SV_TARGET
 {
-    return float4(input.Pos.xyz, 1.0);
+    
 }
