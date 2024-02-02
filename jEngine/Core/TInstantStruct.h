d115b2a4fafdc5490319d4b8dcfbe7cd1d1f5f73
scahp <scahp@naver.com>
2024-02-03 00:27:11 +0900

    Bugfix of hash generation
    Because compiler added padding to the struct. the struct pack(1) is needed.

--- jEngine/Core/TInstantStruct.h
+++ jEngine/Core/TInstantStruct.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
 
+// * Struct could be added padding by compiler and it is filled with trash data.
+//   so, packing rule set to 1 here. If this code removed, you can see the different hash with same struct data.
+#pragma pack(push, 1)
+
 // jInstantStructInternal
 // Recursive declaration of variables that passed into variadic template arguments
 template<typename... Args>
@@ -12,7 +16,7 @@ struct jInstantStructInternal<T, Args...> : public jInstantStructInternal<Args..
 public:
 	static_assert(std::is_trivially_copyable<T>::value, "InstanceStruct members should be 'trivially copyable'");
 	static_assert(!std::is_pointer<T>::value, "InstanceStruct members should not be 'pointer'");
-	T Data{};
+    T Data{};
 	jInstantStructInternal<T, Args...>(T v, Args... args) : Data(v), jInstantStructInternal<Args...>(args...) {}
 };
 
@@ -30,6 +34,8 @@ struct jInstantStruct : public jInstantStructInternal<Args...>
 	jInstantStruct(Args... args) : jInstantStructInternal<Args...>(args...) {}
 };
 
+#pragma pack(pop)
+
 // Easy hash generation from InstantStruct, it is slow calling 'Hash Generation Function' for each variables Indivisually.
 #define GETHASH_FROM_INSTANT_STRUCT(...) \
-[&]() -> uint64 { auto InstanceStruct = jInstantStruct(__VA_ARGS__); return CityHash64((const char*)&InstanceStruct, sizeof(InstanceStruct)); }()
+[&]() -> uint64 { auto InstanceStruct = jInstantStruct(__VA_ARGS__); return XXH64(&InstanceStruct, sizeof(InstanceStruct), 0); }()
